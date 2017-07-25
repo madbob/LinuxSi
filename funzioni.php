7376863be4d1fa068283081627ff616ed8e76d4f
Roberto Guido <bob4job@gmail.com>
2017-07-26 00:51:43 +0200

    ripristinato codice Piwik

--- funzioni.php
+++ funzioni.php
@@ -339,6 +339,23 @@ function lugfooter () {
 	</div>
 </div>
 
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['disableCookies']);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//pergamena.lugbs.linux.it/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 10]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//pergamena.lugbs.linux.it/piwik.php?idsite=10" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+
 </body>
 </html>
 
